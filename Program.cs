a1959226d7cea3cb576dbd386281de93e25a1c16
Sosa <fh22910@gmail.com>
2024-03-16 10:56:37 -0600

    Ejercicio 6

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿
+using System.Diagnostics.CodeAnalysis;
+using System.Reflection.PortableExecutable;
+
+static void ejemplo1()
+{
+    int[] calificaciones;
+    calificaciones = new int[] { 85, 92, 77, 68, 96,89,85,99,95, 80};
+    Array.Sort(calificaciones);
+    Console.WriteLine(calificaciones[2]);
+    double sum = 0;
+    int cantidad = calificaciones.Length;
+    foreach (int numero in calificaciones)
+    {
+        Console.WriteLine(numero);
+        sum += numero;       
+    }
+        double promedio= (double)sum / cantidad;
+    Console.WriteLine("\nPromedio {0}\n", promedio);
+    for (int i = 0; i < calificaciones.Length; i++)
+    {
+        Console.WriteLine($"Calificacion {i + 1}: {calificaciones[i]}");
+    }
+    bool hayaprobados = Array.Exists(calificaciones, calif => calif >= 60);
+    Console.WriteLine(hayaprobados);
+
+    int primernumero= Array.Find (calificaciones, num=> num )
+
+}
+ejemplo1();
